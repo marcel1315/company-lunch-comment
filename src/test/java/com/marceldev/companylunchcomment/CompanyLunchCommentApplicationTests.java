ce1a869ac3893f0f11c145e6a969762da3994e19
Marcel Lee <chungchung1315@gmail.com>
2024-05-19 13:53:22 +0900

    chore: Init project
    Spring initializer를 사용해 프로젝트 시작
    README.md 작성
    ERD 작성
    pull_request_template.md 추가

---
+++ src/test/java/com/marceldev/companylunchcomment/CompanyLunchCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceldev.companylunchcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyLunchCommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
