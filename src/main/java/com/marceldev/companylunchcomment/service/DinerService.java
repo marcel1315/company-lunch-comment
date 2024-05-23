a09b04e7040cc529a955686f8635ab35b59c8daa
Marcel Lee <chungchung1315@gmail.com>
2024-05-23 12:53:30 +0900

    feat: Limit max image count

--- src/main/java/com/marceldev/companylunchcomment/service/DinerService.java
+++ src/main/java/com/marceldev/companylunchcomment/service/DinerService.java
@@ -11,22 +11,27 @@ import com.marceldev.companylunchcomment.exception.DinerNotFoundException;
 import com.marceldev.companylunchcomment.exception.DuplicateDinerTagException;
 import com.marceldev.companylunchcomment.exception.ImageUploadFail;
 import com.marceldev.companylunchcomment.exception.InternalServerError;
+import com.marceldev.companylunchcomment.exception.DinerMaxImageCountExceedException;
 import com.marceldev.companylunchcomment.repository.DinerImageRepository;
 import com.marceldev.companylunchcomment.repository.DinerRepository;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DinerService {
 
-  private static final Logger log = LoggerFactory.getLogger(DinerService.class);
+  @Value("${s3.diner-max-image-count}")
+  private int dinerMaxImageCount;
+
   private final DinerRepository dinerRepository;
+
   private final DinerImageRepository dinerImageRepository;
 
   private final S3Manager s3Manager;
@@ -85,20 +90,18 @@ public class DinerService {
 
   /**
    * 식당 사진 추가
-   * 사진의 순서는 가장 큰 값에 +1을 함
+   * 사진의 순서값은 이미 있는 사진의 가장 큰 값에 +100씩 함
    */
   public void addDinerImage(long id, MultipartFile file) {
     Diner diner = getDiner(id);
 
     try {
+      checkMaxImageCount(diner);
+
       String key = s3Manager.uploadFile(id, file);
-      int order = dinerImageRepository.findTopByDinerOrderByOrdersDesc(diner)
-          .map(DinerImage::getOrders)
-          .map((value) -> value + 1)
-          .orElse(0);
       DinerImage dinerImage = DinerImage.builder()
           .link(key)
-          .orders(order)
+          .orders(getNextImageOrder(diner))
           .diner(diner)
           .build();
       dinerImageRepository.save(dinerImage);
@@ -119,6 +122,23 @@ public class DinerService {
       dinerRepository.save(diner);
     } catch (RuntimeException e) {
       throw new InternalServerError("식당 저장 실패");
+    }
+  }
+
+  private int getNextImageOrder(Diner diner) {
+    // 1개씩 order가 붙어있으면 order 수정시에 여러 image들의 order를 수정해야하므로 간격을 줌
+    int orderStep = 100;
+
+    return dinerImageRepository.findTopByDinerOrderByOrdersDesc(diner)
+        .map(DinerImage::getOrders)
+        .map((value) -> value + orderStep)
+        .orElse(orderStep);
+  }
+
+  private void checkMaxImageCount(Diner diner) {
+    int count = dinerImageRepository.countByDiner(diner);
+    if (count >= dinerMaxImageCount) {
+      throw new DinerMaxImageCountExceedException();
     }
   }
 }
