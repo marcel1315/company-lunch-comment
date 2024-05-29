724bd53fdc64d756a86e295fdf4815403d8ac835
Marcel Lee <chungchung1315@gmail.com>
2024-05-30 01:36:36 +0900

    feat: Constrain api path

--- src/main/java/com/marceldev/companylunchcomment/config/SecurityConfiguration.java
+++ src/main/java/com/marceldev/companylunchcomment/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.marceldev.companylunchcomment.config;
 
 import com.marceldev.companylunchcomment.filter.JwtAuthenticationFilter;
-import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +20,6 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    //TODO: Set permit constraint after making Member entity
     http
         .csrf(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable)
@@ -30,7 +28,8 @@ public class SecurityConfiguration {
         .authorizeHttpRequests(authorization -> authorization
             .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
                 "/swagger-resources/**", "/webjars/**").permitAll() // Swagger UI
-            .requestMatchers("/**").permitAll()
+            .requestMatchers("/signup", "/signin").permitAll()
+            .requestMatchers("/diner/**").authenticated()
             .anyRequest().authenticated()
         )
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
