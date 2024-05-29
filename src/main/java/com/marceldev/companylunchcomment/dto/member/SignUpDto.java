ef891fd0955373f7800b42c20af7d9917644a2c9
Marcel Lee <chungchung1315@gmail.com>
2024-05-30 01:46:16 +0900

    feat: Add validation to signup, signin dto

--- src/main/java/com/marceldev/companylunchcomment/dto/member/SignUpDto.java
+++ src/main/java/com/marceldev/companylunchcomment/dto/member/SignUpDto.java
@@ -1,6 +1,9 @@
 package com.marceldev.companylunchcomment.dto.member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,11 +12,16 @@ import lombok.Data;
 public class SignUpDto {
 
   @NotNull
+  @Email
+  @Schema(example = "hello@company.com")
   private String email;
 
   @NotNull
+  @Schema(description = "비밀번호는 8~50자. 영문자, 숫자 포함", example = "secretpw12")
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,50}$")
   private String password;
 
   @NotNull
+  @Schema(example = "이영수")
   private String name;
 }
