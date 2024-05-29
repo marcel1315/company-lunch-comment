5db9b9b353debdac95f5f1d8e808cb4dfd7b9103
Marcel Lee <chungchung1315@gmail.com>
2024-05-30 02:22:44 +0900

    docs: Add swagger authentication

--- src/main/java/com/marceldev/companylunchcomment/config/SwaggerConfiguration.java
+++ src/main/java/com/marceldev/companylunchcomment/config/SwaggerConfiguration.java
@@ -1,15 +1,21 @@
 package com.marceldev.companylunchcomment.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
     info = @Info(
         title = "직장인 점심 코멘트 서비스",
         version = "1.0",
         description = "점심 코멘트를 기록하고, 사내에 공유하는 서비스입니다"
-    )
+    ),
+    security = @SecurityRequirement(name = "BearerAuth")
 )
+@SecurityScheme(name = "BearerAuth", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class SwaggerConfiguration {
 
 }
