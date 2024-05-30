5df0e69b1a0cfed822427cdd0256b754db6df935
Marcel Lee <chungchung1315@gmail.com>
2024-05-31 00:12:17 +0900

    refactor: Introduce dto for token response

---
+++ src/main/java/com/marceldev/companylunchcomment/dto/member/TokenDto.java
@@ -0,0 +1,11 @@
+package com.marceldev.companylunchcomment.dto.member;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class TokenDto {
+
+  private final String token;
+}
