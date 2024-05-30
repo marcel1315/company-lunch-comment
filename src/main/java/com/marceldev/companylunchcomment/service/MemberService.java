b5114e0a0d265ba8664c3f8a717b3fbb669134fc
Marcel Lee <chungchung1315@gmail.com>
2024-05-30 13:32:26 +0900

    feat: Send email for verification code

--- src/main/java/com/marceldev/companylunchcomment/service/MemberService.java
+++ src/main/java/com/marceldev/companylunchcomment/service/MemberService.java
@@ -1,13 +1,18 @@
 package com.marceldev.companylunchcomment.service;
 
+import com.marceldev.companylunchcomment.component.EmailSender;
 import com.marceldev.companylunchcomment.component.TokenProvider;
+import com.marceldev.companylunchcomment.dto.member.SendVerificationCodeDto;
 import com.marceldev.companylunchcomment.dto.member.SignInDto;
 import com.marceldev.companylunchcomment.dto.member.SignUpDto;
 import com.marceldev.companylunchcomment.entity.Member;
+import com.marceldev.companylunchcomment.exception.EmailIsNotCompanyDomain;
 import com.marceldev.companylunchcomment.exception.IncorrectPasswordException;
 import com.marceldev.companylunchcomment.exception.MemberNotExistException;
 import com.marceldev.companylunchcomment.repository.MemberRepository;
 import com.marceldev.companylunchcomment.type.Role;
+import java.util.HashSet;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,14 @@ public class MemberService {
   private final PasswordEncoder passwordEncoder;
 
   private final TokenProvider tokenProvider;
+
+  private final EmailSender emailSender;
+
+  private final HashSet<String> NOT_SUPPORTED_DOMAINS = new HashSet<>(List.of(
+      "naver.com", "gmail.com", "daum.net", "kakao.com", "hanmail.net", "yahoo.com",
+      "nate.com", "hotmail.com", "outlook.com", "live.com", "msn.com",
+      "icloud.com", "me.com", "aol.com", "protonmail.com"
+  ));
 
   /**
    * 회원가입
@@ -51,5 +64,16 @@ public class MemberService {
     }
 
     return tokenProvider.generateToken(member.getEmail(), Role.USER.toString());
+  }
+
+  public void sendVerificationCode(SendVerificationCodeDto dto) {
+    String domain = dto.getEmail().split("@")[1];
+    if (NOT_SUPPORTED_DOMAINS.contains(domain)) {
+      throw new EmailIsNotCompanyDomain(domain);
+    }
+
+    String subject = "[Company Lunch Comment] 회원가입을 위한 인증번호입니다";
+    String body = "인증번호는 000000 입니다. 회원가입란에 입력해주세요.";
+    emailSender.sendMail(dto.getEmail(), subject, body);
   }
 }
