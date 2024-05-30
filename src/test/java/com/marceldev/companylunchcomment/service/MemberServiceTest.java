d77874073bc2244eb39efb93800c497d8dccf95c
Marcel Lee <chungchung1315@gmail.com>
2024-05-30 18:09:09 +0900

    test: Send verification code for signing up

--- src/test/java/com/marceldev/companylunchcomment/service/MemberServiceTest.java
+++ src/test/java/com/marceldev/companylunchcomment/service/MemberServiceTest.java
@@ -3,19 +3,29 @@ package com.marceldev.companylunchcomment.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.marceldev.companylunchcomment.component.EmailSender;
 import com.marceldev.companylunchcomment.component.TokenProvider;
+import com.marceldev.companylunchcomment.component.VerificationCodeGenerator;
+import com.marceldev.companylunchcomment.dto.member.SendVerificationCodeDto;
 import com.marceldev.companylunchcomment.dto.member.SignInDto;
 import com.marceldev.companylunchcomment.dto.member.SignUpDto;
 import com.marceldev.companylunchcomment.entity.Member;
+import com.marceldev.companylunchcomment.entity.SignupVerification;
+import com.marceldev.companylunchcomment.exception.AlreadyExistMemberException;
+import com.marceldev.companylunchcomment.exception.EmailIsNotCompanyDomain;
 import com.marceldev.companylunchcomment.exception.IncorrectPasswordException;
 import com.marceldev.companylunchcomment.exception.MemberNotExistException;
 import com.marceldev.companylunchcomment.repository.MemberRepository;
+import com.marceldev.companylunchcomment.repository.SignupVerificationRepository;
 import com.marceldev.companylunchcomment.type.Role;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +48,15 @@ class MemberServiceTest {
   @Mock
   private TokenProvider tokenProvider;
 
+  @Mock
+  private EmailSender emailSender;
+
+  @Mock
+  private VerificationCodeGenerator verificationCodeGenerator;
+
+  @Mock
+  private SignupVerificationRepository signupVerificationRepository;
+
   @InjectMocks
   private MemberService memberService;
 
@@ -47,9 +66,18 @@ class MemberServiceTest {
     //given
     SignUpDto dto = SignUpDto.builder()
         .email("hello@example.com")
-        .password("a1234")
+        .password("abc123123")
         .name("이영수")
+        .verificationCode("123123")
         .build();
+
+    when(signupVerificationRepository.findByEmail(any()))
+        .thenReturn(Optional.of(SignupVerification.builder()
+            .email("hello@example.com")
+            .code("123123")
+            .expiration(LocalDateTime.now().plusSeconds(30))
+            .build()
+        ));
 
     //when
     memberService.signUp(dto);
@@ -58,7 +86,7 @@ class MemberServiceTest {
     //then
     verify(memberRepository).save(captor.capture());
     assertEquals(captor.getValue().getEmail(), "hello@example.com");
-    assertNotEquals(captor.getValue().getPassword(), "a1234");
+    assertNotEquals(captor.getValue().getPassword(), "abc123123");
     assertEquals(captor.getValue().getName(), "이영수");
     verify(passwordEncoder).encode(any());
   }
@@ -149,5 +177,58 @@ class MemberServiceTest {
     //then
     assertThrows(RuntimeException.class,
         () -> memberService.signIn(dto));
+  }
+
+  @Test
+  @DisplayName("인증번호 이메일 전송 - 성공")
+  void send_verification_code() {
+    //given
+    SendVerificationCodeDto dto = new SendVerificationCodeDto();
+    dto.setEmail("hello@example.com");
+
+    when(memberRepository.existsByEmail(any()))
+        .thenReturn(false);
+    when(verificationCodeGenerator.generate(anyInt()))
+        .thenReturn("123123");
+
+    ArgumentCaptor<String> captor1 = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<String> captor2 = ArgumentCaptor.forClass(String.class);
+
+    //when
+    memberService.sendVerificationCode(dto);
+
+    //then
+    verify(emailSender).sendMail(captor1.capture(), any(), captor2.capture());
+    assertEquals(captor1.getValue(), "hello@example.com");
+    assertTrue(captor2.getValue().contains("123123"));
+  }
+
+  @Test
+  @DisplayName("인증번호 이메일 전송 - 실패(이메일 공급자의 이메일임)")
+  void send_verification_code_fail_email_provider_domain() {
+    //given
+    SendVerificationCodeDto dto = new SendVerificationCodeDto();
+    dto.setEmail("hello@gmail.com");
+
+    //when
+    //then
+    assertThrows(EmailIsNotCompanyDomain.class,
+        () -> memberService.sendVerificationCode(dto));
+  }
+
+  @Test
+  @DisplayName("인증번호 이메일 전송 - 실패(이미 존재하는 회원)")
+  void send_verification_code_fail_already_member() {
+    //given
+    SendVerificationCodeDto dto = new SendVerificationCodeDto();
+    dto.setEmail("hello@example.com");
+
+    when(memberRepository.existsByEmail(any()))
+        .thenReturn(true);
+
+    //when
+    //then
+    assertThrows(AlreadyExistMemberException.class,
+        () -> memberService.sendVerificationCode(dto));
   }
 }
