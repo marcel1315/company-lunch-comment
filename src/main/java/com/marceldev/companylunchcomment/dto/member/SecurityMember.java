ecf32abea1a01710af7fee6d544208a30d45ea99
Marcel Lee <chungchung1315@gmail.com>
2024-06-01 01:55:13 +0900

    refactor: Replace UserDetailsService of spring security
    - Remove native query
    - Use jpa findByEmail
    - MemberService implements UserDetailsService
    - TokenProvider places in Controller instead of Service

---
+++ src/main/java/com/marceldev/companylunchcomment/dto/member/SecurityMember.java
@@ -0,0 +1,51 @@
+package com.marceldev.companylunchcomment.dto.member;
+
+import com.marceldev.companylunchcomment.entity.Member;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class SecurityMember implements UserDetails {
+
+  private final Member member;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(member.getRole().toString());
+    return List.of(authority);
+  }
+
+  @Override
+  public String getPassword() {
+    return member.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return member.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
