63d2c60bb3c7f8c89044dd3b90943c3864c4cf3c
Marcel Lee <chungchung1315@gmail.com>
2024-06-04 00:21:39 +0900

    refactor: Use util static method for simple extract

---
+++ src/main/java/com/marceldev/companylunchcomment/util/ExtractDomain.java
@@ -0,0 +1,14 @@
+package com.marceldev.companylunchcomment.util;
+
+import com.marceldev.companylunchcomment.exception.InvalidEmailFormatException;
+
+public class ExtractDomain {
+
+  public static String from(String email) {
+    String[] split = email.split("@");
+    if (split.length != 2 || split[0].isEmpty() || split[1].isEmpty()) {
+      throw new InvalidEmailFormatException(email);
+    }
+    return split[1];
+  }
+}
