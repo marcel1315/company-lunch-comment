5f125ed43ec410c99f7dd1a3df325c1e45ebd31a
Marcel Lee <chungchung1315@gmail.com>
2024-06-03 19:43:13 +0900

    test: Update comments

--- src/main/java/com/marceldev/companylunchcomment/dto/comments/UpdateCommentsDto.java
+++ src/main/java/com/marceldev/companylunchcomment/dto/comments/UpdateCommentsDto.java
@@ -3,9 +3,11 @@ package com.marceldev.companylunchcomment.dto.comments;
 import com.marceldev.companylunchcomment.type.ShareStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdateCommentsDto {
 
   @NotNull
