d62940af8c880158a03838c2e0a4b4fd705fef30
Marcel Lee <chungchung1315@gmail.com>
2024-06-04 21:01:43 +0900

    feat: Add reply entity

---
+++ src/main/java/com/marceldev/companylunchcomment/entity/Reply.java
@@ -0,0 +1,47 @@
+package com.marceldev.companylunchcomment.entity;
+
+import com.marceldev.companylunchcomment.type.ShareStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString(exclude = {"member", "comments"})
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Reply {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Setter
+  @Column(length = 300, nullable = false)
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "comments_id", nullable = false)
+  private Comments comments;
+}
