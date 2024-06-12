085d8952f1995a99312f060ee3b69bb17c9dc10c
Marcel Lee <chungchung1315@gmail.com>
2024-06-13 00:48:45 +0900

    fix: Change client error code more specific

--- src/main/java/com/marceldev/companylunchcomment/exception/MemberNotExistException.java
+++ src/main/java/com/marceldev/companylunchcomment/exception/MemberNotExistException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class MemberNotExistException extends CustomException {
 
   public MemberNotExistException() {
-    super(HttpStatus.BAD_REQUEST, "Member doesn't exist.");
+    super(HttpStatus.NOT_FOUND, "Member doesn't exist.");
   }
 }
