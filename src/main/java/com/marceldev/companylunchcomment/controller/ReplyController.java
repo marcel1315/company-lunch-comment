28bd2df653194a033d5c2ef670d113c9e3bf9b4b
Marcel Lee <chungchung1315@gmail.com>
2024-06-12 23:46:21 +0900

    fix: Change to PUT method on updating behavior

--- src/main/java/com/marceldev/companylunchcomment/controller/ReplyController.java
+++ src/main/java/com/marceldev/companylunchcomment/controller/ReplyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,7 +57,7 @@ public class ReplyController {
       description = "사용자는 코멘트에 댓글 수정이 가능하다.<br>"
           + "수정은 자신이 작성한 댓글만 가능하다."
   )
-  @PostMapping("/diners/{dinerId}/comments/{commentsId}/replies/{replyId}")
+  @PutMapping("/diners/{dinerId}/comments/{commentsId}/replies/{replyId}")
   public CustomResponse<?> updateReply(
       @PathVariable long replyId,
       @Validated @RequestBody UpdateReplyDto updateReplyDto
