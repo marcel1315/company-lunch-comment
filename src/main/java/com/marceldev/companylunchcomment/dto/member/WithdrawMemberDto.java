93cad12226893af3446c3ad8974b4398a72fc8db
Marcel Lee <chungchung1315@gmail.com>
2024-06-12 16:54:43 +0900

    test: Update member
    - Change name
    - Change password
    - Withdraw

--- src/main/java/com/marceldev/companylunchcomment/dto/member/WithdrawMemberDto.java
+++ src/main/java/com/marceldev/companylunchcomment/dto/member/WithdrawMemberDto.java
@@ -2,9 +2,11 @@ package com.marceldev.companylunchcomment.dto.member;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class WithdrawMemberDto {
 
   @NotNull
