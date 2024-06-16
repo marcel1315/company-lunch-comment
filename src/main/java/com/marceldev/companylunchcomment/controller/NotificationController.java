68920cf067ab2f097147b313ba78c8733d236d24
Marcel Lee <chungchung1315@gmail.com>
2024-06-16 11:06:06 +0900

    fix: Update security configuration for notification

--- src/main/java/com/marceldev/companylunchcomment/controller/NotificationController.java
+++ src/main/java/com/marceldev/companylunchcomment/controller/NotificationController.java
@@ -23,7 +23,7 @@ public class NotificationController {
       summary = "SSE 커넥션 맺기",
       description = "최대 연결시간은 30분이다. 30분이 지나서 끊기면 재연결이 필요하다."
   )
-  @GetMapping("/notification/sse")
+  @GetMapping("/notifications/sse")
   public SseEmitter notificationSse() {
     return notificationService.createEmitter();
   }
@@ -33,7 +33,7 @@ public class NotificationController {
       description = "클라이언트가 더 이상 연결하지 않으려면 명시적으로 끊어야 한다.<br>"
           + "서버에서 keep-alive를 15초 간격으로 호출해서 끊긴 것을 감지하지만, 15초 동안 있다고 가정하고 SSE 알림을 시도한다."
   )
-  @PostMapping("/notification/sse/terminate")
+  @PostMapping("/notifications/sse/terminate")
   public CustomResponse<?> notificationSseTerminate() {
     notificationService.removeEmitter();
     return CustomResponse.success();
@@ -43,7 +43,7 @@ public class NotificationController {
       summary = "FCM Push Notification Token 전달",
       description = "클라이언트가 발급받은 FCM Push Notificaton Token을 서버로 전달한다."
   )
-  @PostMapping("/notification/fcm/token")
+  @PostMapping("/notifications/fcm/token")
   public CustomResponse<?> notificationFcmToken(@RequestBody String token) {
     notificationService.registerToken(token);
     return CustomResponse.success();
@@ -53,7 +53,7 @@ public class NotificationController {
       summary = "FCM Push Notification Token 삭제",
       description = "클라이언트가 발급받은 FCM Push Notification Token을 서버에서 삭제한다."
   )
-  @DeleteMapping("/notification/fcm/token")
+  @DeleteMapping("/notifications/fcm/token")
   public CustomResponse<?> notificationFcmTokenDelete() {
     notificationService.unregisterToken();
     return CustomResponse.success();
