65a4e964d7c0cfa14ee8c00d673d301b47bc57c4
Marcel Lee <chungchung1315@gmail.com>
2024-06-17 15:42:09 +0900

    docs: Update swagger title

--- src/main/java/com/marceldev/companylunchcomment/config/SwaggerConfiguration.java
+++ src/main/java/com/marceldev/companylunchcomment/config/SwaggerConfiguration.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
     info = @Info(
-        title = "직장인 점심 코멘트 서비스",
+        title = "우리 회사 점심 - 직장인 점심 기록 서비스 백엔드",
         version = "1.0",
         description = "점심 코멘트를 기록하고, 사내에 공유하는 서비스입니다"
     ),
